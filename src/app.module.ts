2629e2e6664509c5025407ca70528bc42c7aff87
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-05-25 20:41:54 +0300

    build: create app

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ComponentsModule } from './components/components.module';
+import { DatabaseModule } from './database/database.module';
+
+@Module({
+  controllers: [AppController],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: process.env.NODE_ENV === 'local' || false,
+      autoSchemaFile: true,
+    }),
+    ComponentsModule,
+  ],
+  providers: [AppService],
+})
+export class AppModule {}
