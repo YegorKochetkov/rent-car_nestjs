aa7e0d36fb129db99919df9ab488214881518e1b
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-06-07 12:06:12 +0300

    feat: add default cars to db

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { Cars } from 'src/helpers/cars';
 import { CarsModule } from './cars/cars.module';
+import { CarsService } from './cars/cars.service';
 
 @Module({
   imports: [CarsModule],
 })
-export class ComponentsModule {}
+export class ComponentsModule {
+  constructor(private carsService: CarsService) {
+    for (const car of Cars) {
+      this.carsService.addCar(car);
+    }
+  }
+}
