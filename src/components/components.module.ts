0952093103df435bce05161f5a866da1245518dc
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-06-08 12:10:56 +0300

    feat: remove adding mock cars

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,15 +1,7 @@
 import { Module } from '@nestjs/common';
-import { Cars } from 'src/helpers/cars';
 import { CarsModule } from './cars/cars.module';
-import { CarsService } from './cars/cars.service';
 
 @Module({
   imports: [CarsModule],
 })
-export class ComponentsModule {
-  constructor(private carsService: CarsService) {
-    for (const car of Cars) {
-      this.carsService.addCar(car);
-    }
-  }
-}
+export class ComponentsModule {}
