2cb03e8d8ae75d4e90ee596639cca61c5bdfd785
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-06-13 22:56:03 +0300

    feat: add cors options

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors(corsOptions);
   app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(process.env.PORT || 9000);
